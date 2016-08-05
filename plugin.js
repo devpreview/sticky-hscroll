dc0ad88df9e1a6a55d82766fb388c7d9cddbb32f
Alexey Savchuk <sabo@devpreview.ru>
2016-08-05 20:23:18 +0300

    Many scroll elements

--- plugin.js
+++ plugin.js
@@ -1,8 +1,4 @@
 (function ($) {
-    var scrollbar = $('<div class="table-responsive-hscroll"><div></div></div>');
-    var fakecontent = scrollbar.find('div');
-    var lastScroll;
-    var container;
 
     function top(e) {
         return e.offset().top;
@@ -12,75 +8,45 @@
         return e.offset().top + e.height();
     }
 
-    var active = $([]);
-    function find_active() {
+    function onscroll(element, scrollbar, scrollLeft) {
         scrollbar.show();
-        var active = $([]);
-        container.find('.table-responsive').each(function () {
-            if (top($(this)) < top(scrollbar) && bottom($(this)) > bottom(scrollbar)) {
-                fakecontent.width($(this).get(0).scrollWidth);
-                fakecontent.height(1);
-                active = $(this);
-            }
-        });
-        fit(active);
-        return active;
-    }
-
-    function fit(active) {
-        if (!active.length)
-            return scrollbar.hide();
-        scrollbar.css({left: active.offset().left, width: active.outerWidth()});
-        fakecontent.width($(this).get(0).scrollWidth);
-        fakecontent.height(1);
-        delete lastScroll;
-    }
-
-    function onscroll() {
-        var oldactive = active;
-        active = find_active();
-        if (oldactive.not(active).length) {
-            oldactive.unbind('scroll', update);
+        if (top(element) < top(scrollbar) && bottom(element) > bottom(scrollbar)) {
+            scrollbar.find('div').css('width', element.get(0).scrollWidth + 'px');
+            scrollbar.css({left: element.offset().left, width: element.outerWidth()});
+            scrollbar.scrollLeft(scrollLeft);
+        } else {
+            scrollbar.hide();
         }
-        if (active.not(oldactive).length) {
-            active.scroll(update);
-        }
-        update();
-    }
-
-
-    function scroll() {
-        if (!active.length)
-            return;
-        if (scrollbar.scrollLeft() === lastScroll)
-            return;
-        lastScroll = scrollbar.scrollLeft();
-        active.scrollLeft(lastScroll);
-    }
-
-    function update() {
-        if (!active.length)
-            return;
-        if (active.scrollLeft() === lastScroll)
-            return;
-        lastScroll = active.scrollLeft();
-        scrollbar.scrollLeft(lastScroll);
     }
 
     $.fn.stickyHScroll = function () {
-        container = this;
-        scrollbar.appendTo($(document.body));
-        scrollbar.hide();
-        scrollbar.css({
-            overflowX: 'auto',
-            position: 'fixed',
-            width: '100%',
-            bottom: 0
+        this.find('.table-responsive').each(function () {
+            var element = $(this);
+            var scrollbar = $('<div class="table-responsive-hscroll"><div></div></div>');
+            var scrollLeft = 0;
+            scrollbar.appendTo($(document.body));
+            scrollbar.hide();
+            scrollbar.css({
+                overflowX: 'auto',
+                position: 'fixed',
+                width: '100%',
+                bottom: 0
+            });
+            scrollbar.find('div').css('height', '0.1px');
+            scrollbar.scroll(function () {
+                element.scrollLeft(scrollbar.scrollLeft());
+            });
+            element.scroll(function () {
+                scrollLeft = element.scrollLeft();
+            });
+            onscroll(element, scrollbar, scrollLeft);
+            $(window).scroll(function () {
+                onscroll(element, scrollbar, scrollLeft);
+            });
+            $(window).resize(function () {
+                onscroll(element, scrollbar, scrollLeft);
+            });
         });
-        scrollbar.scroll(scroll);
-        onscroll();
-        $(window).scroll(onscroll);
-        $(window).resize(onscroll);
         return this;
     };
 }(jQuery));
